775cd31ee5701c645dbacd5449131a6f4acad9e0
Ridter <zuotonghk@gmail.com>
2015-03-24 13:58:22 +0800

    port scan

--- scanport.py
+++ scanport.py
@@ -3,6 +3,7 @@
 from socket import *
 import os
 import sys
+Timeout = 2.0 
 def gethost(fileroute):
     hosts=[]
     fileobject = open(fileroute,'r')
@@ -13,10 +14,8 @@ def gethost(fileroute):
         for line in lines:
             hosts.append(line.strip('\n'))
     return hosts
-filename= sys.argv[0]
-if len(sys.argv)<3:
-    print 'usage:  ' + filename + '  iplist.txt port'
-else:
+
+def scanport():
     hosts = gethost(sys.argv[1])
     PORT = int(sys.argv[2])
     fileobject = open("result.txt",'w')
@@ -25,12 +24,24 @@ else:
     for host in hosts:     
         try:
             tcpCliSock = socket(AF_INET,SOCK_STREAM)
+            tcpCliSock.settimeout(Timeout)
             tcpCliSock.connect((host,PORT))
             tcpCliSock.close()
             del tcpCliSock
-            print "try:" +host+" ->"+str(PORT) 
+            print host+" ->"+str(PORT) +"is Open!"
             result = host+'\n'
             fileobject.writelines([result])
         except error:
-            print host+'not connect'
+            print host+' not connect'
             continue
+def main():
+    filename= sys.argv[0]
+    if len(sys.argv)<3:
+        print 'usage:  ' + filename + '  iplist.txt port'
+    else:
+        scanport()
+if __name__=='__main__':
+    try:
+        main()
+    except:
+        print "Error"
